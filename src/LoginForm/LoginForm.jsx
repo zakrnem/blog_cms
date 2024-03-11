c67a2dcbbaee15591c6851db37480f80f81da620
zakrnem <zakrnem@zohomail.com>
2024-03-11 13:52:06 -0600

    Implement login form

--- src/LoginForm/LoginForm.jsx
+++ src/LoginForm/LoginForm.jsx
@@ -1,9 +1,30 @@
+import styles from './LoginForm.module.css'
+
 function LoginForm() {
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log("Submit")
+  }
     return (
-        <>
-            <h2>Login form</h2>
-        </>
-    )
+        <div className={styles.login}>
+          <div className={styles.title}>Log in to your account</div>
+          <form onSubmit={handleSubmit} className={styles.form}>
+            <div>
+              <label htmlFor="username">Username: </label>
+              <input type="text" id="username" placeholder="Enter your username" />
+            </div>
+            <div>
+              <label htmlFor="password">Password: </label>
+              <input
+                type="password"
+                id="password"
+                placeholder="Enter your password"
+              />
+            </div>
+            <input className={styles.submit} type="submit" />
+          </form>
+        </div>
+      );
 }
 
 export default LoginForm
